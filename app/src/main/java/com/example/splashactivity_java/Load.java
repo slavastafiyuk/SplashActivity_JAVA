cd164ad026c63e5e4b9019210b2b66ca19e7c34a
slavastafiyuk <slavast@outlook.pt>
2021-10-11 21:01:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/splashactivity_java/Load.java
@@ -0,0 +1,33 @@
+package com.example.splashactivity_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Load extends AppCompatActivity {
+    Thread timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this){
+                        wait(5000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Load.this, Iniciar.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
